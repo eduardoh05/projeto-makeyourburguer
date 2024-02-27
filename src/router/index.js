d5a356388eeeb888ad2170de4f7712dd280522bf
Eduardo Henrique Alves <ealves@PGE.MS>
2024-02-27 11:35:09 -0400

    Método de enviar dados para o backend finalizado no formulário

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '@/views/Home.vue'
+import Pedidos from '@/views/Pedidos.vue'
+
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path:'/',
+    name: 'Home',
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/pedidos',
+    name: 'Pedidos',
+    component: Pedidos
   }
+
 ]
 
 const router = createRouter({
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <Banner/>
+  <img src="" alt="">
+  <div class="main-container">
+    <h1>Monte seu burguer:</h1>
+    <burguer-form/>
+  </div>
+  
+</template>
+
+<script>
+import BurguerForm from '@/components/BurguerForm.vue'
+import Banner from '../components/Banner.vue'
+export default {
+  name:"Home",
+  components:{
+    Banner,
+    BurguerForm
+  }
+
+}
+</script>
+
+<style>
+
+</style>
