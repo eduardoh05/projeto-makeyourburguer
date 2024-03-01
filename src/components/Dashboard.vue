e1cb0e87b5fae5a75286c089cf9919ab655b8e16
Eduardo Henrique Alves <ealves@PGE.MS>
2024-03-01 09:22:22 -0400

    Atualização referente ao status do hambúrguer

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -26,7 +26,7 @@
                 <div>
                     <select name="status" class="status">
                         <option value="">Selecione</option>
-                        <option v-for="s in status" :key="s.id" value="s.tipo">{{s.tipo}}</option>
+                        <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">{{s.tipo}}</option>
                     </select>
 
                     <button class="delete-btn">Cancelar</button>
@@ -61,7 +61,7 @@ export default {
             console.log(this.burgers)
 
             // resgatar os status
-            this.getStatus()
+            this.getStatus() 
 
         },
 
